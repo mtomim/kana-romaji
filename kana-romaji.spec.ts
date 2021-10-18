5fe5a4ad0dfce4878eb9c72f168d5f9f282e82b5
Masaru Tomimitsu <masaru.tomimitsu@gmail.com>
2021-10-18 13:26:39 +0200

    initial commit

---
+++ kana-romaji.spec.ts
@@ -0,0 +1,40 @@
+import { toRomaji } from "./kana-romaji";
+
+describe('kanaRomaji.ts', () => {
+    test.each([
+        ['ちちゅうかい', 'chichūkai'],
+        ['しんぶんし', 'shimbunshi'],
+        ['いっぱんじん', 'ippanjin'],
+        ['しょうがくせい', 'shōgakusei'],
+        ['おぅ', 'ou'],
+        ['おっ', 'o'],
+        ['かちょう', 'kachō'],
+        ['ひょう', 'hyō'],
+        ['みょう', 'myō'],
+        ['ぎょう', 'gyō'],
+        ['じょう', 'jō'],
+        ['ょゅぅ', 'yoyuu'],
+        ['ひょうご', 'hyōgo'],
+        ['ひゅーご', 'hyūgo'],
+        ['ふぃーちゅあー', 'fīchuā'],
+        ['ふゅーちゃー', 'fyūchā'],
+        ['みふぁそ', 'mifaso'],
+        ['ふらんすご', 'furansugo'],
+        ['ふぁみりー', 'famirī'],
+        ['ふらの', 'furano'],
+        ['ふりょう', 'furyō'],
+        ['おんおふすいっち', 'on-ofusuitchi'],
+        ['こんにゃく', 'konnyaku'],
+        ['あんま', 'amma'],
+        ['おかあさん', 'okāsan'],
+        ['きーきー', 'kīkī'],
+        ['けーき', 'kēki'],
+        ['ケーキ', 'KĒKI'],
+        ['とおり', 'tōri'],
+        ['きいはんとう', 'kīhantō'],
+        ['ろくおんテープ', 'rokuonTĒPU'],
+        ['S.O.S.はエスオーエス', 'S.O.S.haESUŌESU'],
+    ])('translates %s to %s', (input, expected) => {
+        expect(toRomaji(input)).toBe(expected);
+    })
+})
